2d3b18441db16477fd0c9f9e78f3a081e846ba7d
Pablo Arcos <pablo.arcos01@epn.edu.ec>
2024-09-08 20:14:10 -0500

    refactor: Update text-analysis.py to use AzureKeyCredential for authentication

--- Labfiles/07-speech/Python/speaking-clock/speaking-clock.py
+++ Labfiles/07-speech/Python/speaking-clock/speaking-clock.py
@@ -4,6 +4,10 @@ import os
 
 # Import namespaces
 
+import azure.cognitiveservices.speech as speech_sdk
+
+from playsound import playsound
+
 
 def main():
     try:
@@ -15,6 +19,9 @@ def main():
         ai_region = os.getenv('SPEECH_REGION')
 
         # Configure speech service
+
+        speech_config = speech_sdk.SpeechConfig(ai_key, ai_region)
+        print('Ready to use speech services in: ', speech_config.region)
         
 
         # Get spoken input
@@ -30,8 +37,28 @@ def TranscribeCommand():
 
     # Configure speech recognition
 
+    # current_dir = os.getcwd()
+    # audioFile = current_dir + '\\time.wav'
+    # playsound(audioFile)
+    # audio_config = speech_sdk.AudioConfig(filename=audioFile)
+    # speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
+
+    audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+    speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
+    print('Speak now...')
 
     # Process speech input
+
+    speech = speech_recognizer.recognize_once_async().get()
+    if speech.reason == speech_sdk.ResultReason.RecognizedSpeech:
+        command = speech.text
+        print(command)
+    else:
+        print(speech.reason)
+        if speech.reason == speech_sdk.ResultReason.Canceled:
+            cancellation = speech.cancellation_details
+            print(cancellation.reason)
+            print(cancellation.error_details)
 
 
     # Return the command
@@ -44,9 +71,29 @@ def TellTime():
 
 
     # Configure speech synthesis
-    
+
+    speech_config.speech_synthesis_voice_name = "en-GB-LibbyNeural"
+    speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config)
+        
 
     # Synthesize spoken output
+
+    # speak = speech_synthesizer.speak_text_async(response_text).get()
+    # if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+    #     print(speak.reason)
+
+     # Synthesize spoken output
+    responseSsml = " \
+        <speak version='1.0' xmlns='http://www.w3.org/2001/10/synthesis' xml:lang='en-US'> \
+            <voice name='en-GB-LibbyNeural'> \
+                {} \
+                <break strength='weak'/> \
+                Time to end this lab! \
+            </voice> \
+        </speak>".format(response_text)
+    speak = speech_synthesizer.speak_ssml_async(responseSsml).get()
+    if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+        print(speak.reason)
 
 
     # Print the response
